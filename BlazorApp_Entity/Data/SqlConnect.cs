cd88488cf98226c280974c0ae2d4ca1ee63297a0
YuZhiJian <641854024@QQ.COM>
2020-02-03 11:14:42 +0800

    blazorui

--- BlazorApp_Entity/Data/SqlConnect.cs
+++ BlazorApp_Entity/Data/SqlConnect.cs
@@ -1,32 +1,55 @@
 ﻿using BlazorApp_Entity.Server;
+using Blazui.Component.Table;
+using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BlazorApp_Entity.Data
 {
-    public class SqlConnect
+    public class ConnectionInfo
     {
-        public class ConnectionInfo
+        public string IpAdress { get; set; }
+        public string Port { get; set; }
+        public string Account { get; set; }
+        public string Pwd { get; set; }
+        public string Database { get; set; }
+    }
+    public class TableInfo
+    {
+        [TableColumn(Text = "表名")]
+        public string TableName { get; set; }
+        //public string Schema { get; set; }
+        [TableColumn(Text = "表描述")]
+        public string Describe { get; set; }
+    }
+    public class SqlConnect : ComponentBase
+    {
+        protected BTable table;
+
+        public List<TableInfo> tables = new List<TableInfo>();
+        protected int currentPage = 1;
+        protected int pageSize = 5;
+        internal int CurrentPage
         {
-            public string IpAdress { get; set; }
-            public string Port { get; set; }
-            public string Account { get; set; }
-            public string Pwd { get; set; }
-            public string Database { get; set; }
+            get
+            {
+                return currentPage;
+            }
+            set
+            {
+                currentPage = value;
+                tables = GetAllList().Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
+            }
         }
-        public class TableInfo
+        public List<TableInfo> GetAllList()
         {
-            public string TableName { get; set; }
-            public string Schema { get; set; }
-            public string Describe { get; set; }
-        }
-        public List<TableInfo> GetDatabaseList(ConnectionInfo Info)
-        {
-            string connection = $"server={Info.IpAdress};port={Info.Port};uid={Info.Account};pwd={Info.Pwd};database={Info.Database}";
-            var TableList = new DBServer(connection).db.Queryable<TableInfo>(@"select top 1000
+            //string connection = $"server={Info.IpAdress};port={Info.Port};uid={Info.Account};pwd={Info.Pwd};database={Info.Database}";
+            string connection = $"Data Source=LAPTOP-FG0SOM1N;Initial Catalog=test;User ID=sa;Password=123;MultipleActiveResultSets=true";
+            var TableList = new DBServer(connection).db.SqlQueryable<TableInfo>(@"select top 1000
                                     ROW_NUMBER() OVER (ORDER BY a.name) AS No, 
-                                    a.name AS 表名,
-                                    CONVERT(NVARCHAR(100),isnull(g.[value],'-')) AS 说明
-                                    ,c.name 架构名
+                                    a.name AS TableName,
+                                    CONVERT(NVARCHAR(100),isnull(g.[value],'-')) AS Describe
+                                    ,c.name [Schema]
                                     from
                                     sys.tables a left join sys.extended_properties g
                                     on (a.object_id = g.major_id AND g.minor_id = 0)
@@ -34,6 +57,23 @@ namespace BlazorApp_Entity.Data
                 .ToList();
 
             return TableList;
+        }
+        public List<TableInfo> GetDatabaseList(ConnectionInfo Info)
+        {
+            //string connection = $"server={Info.IpAdress};port={Info.Port};uid={Info.Account};pwd={Info.Pwd};database={Info.Database}";
+            string connection = $"Data Source=LAPTOP-FG0SOM1N;Initial Catalog=test;User ID=sa;Password=123;MultipleActiveResultSets=true";
+            var TableList = new DBServer(connection).db.SqlQueryable<TableInfo>(@"select top 1000
+                                    ROW_NUMBER() OVER (ORDER BY a.name) AS No, 
+                                    a.name AS TableName,
+                                    CONVERT(NVARCHAR(100),isnull(g.[value],'-')) AS Describe
+                                    ,c.name [Schema]
+                                    from
+                                    sys.tables a left join sys.extended_properties g
+                                    on (a.object_id = g.major_id AND g.minor_id = 0)
+                                    left join sys.schemas c on a.schema_id=c.schema_id")
+                .ToList();
+
+            return TableList.Skip((currentPage - 1) * pageSize).Take(pageSize).ToList();
         }
     }
 }
